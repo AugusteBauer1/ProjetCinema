4103c6a084f3420d34d3bd184fe06e3011cf452b
Auguste <57798677+PapierBulle1@users.noreply.github.com>
2022-03-24 15:19:38 +0100

    tets
    test

---
+++ cinema/src/cinema/controller/cinemacontroller.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cinema.controller;
+
+/**
+
+/**
+ *
+ * @author augus
+ */
+public class cinemacontroller {
+    public cinemacontroller(String uname, String pw) {
+        // chercher dans la db si il existe
+        // SELECT id FROM user WHERE nom=uname AND pw=pw;
+        // si non le creer 
+    }
+}
+
